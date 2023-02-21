77277ebb365e6e53b88a521353e4ce2c30665c39
Luke Bailey <lukebailey@college.harvard.edu>
2023-02-21 11:58:11 -0500

    Added round robin K dropout module. TODO test this module.

--- k_dropout_modules.py
+++ k_dropout_modules.py
@@ -31,6 +31,43 @@ class StochasticKDropout(nn.Linear):
 
         return Z 
 
+
+class RRKDropout(nn.Linear):
+    def __init__(self, in_feats, out_feats, drop_p, k, num_masks, bias=True):
+        super(RRKDropout, self).__init__(in_feats, out_feats, bias=bias)
+
+        self.p = drop_p
+        self.k = k
+        self.uses = 0
+        self.mask_seeds = torch.randint(high=5000, size=(num_masks,))
+        self.mask_idx = -1
+        self.binomial = torch.distributions.binomial.Binomial(probs=1-self.p)
+        self.mask = None
+
+    def forward(self, input):
+        
+        Z = F.linear(input, self.weight, self.bias)
+
+        if self.training: 
+
+            if self.uses % self.k == 0:
+                # Update index
+                self.mask_idx += 1
+                self.mask_idx %= self.mask_seeds.shape[0]
+
+                # Get same mask as previous mask_idx by seeding RNG
+                torch.manual_seed(self.mask_seeds[self.mask_idx].item())
+                self.mask = self.binomial.sample(Z.size()).to(self.weight.device)
+                self.uses = 0
+
+            self.uses += 1
+
+            # Dropout activations and scale the rest
+            return Z * self.mask * (1.0/(1-self.p))
+
+        return Z 
+
+
 class StochasticKDropoutNet(nn.Module):
 
     def __init__(self, num_classes=2, input_dim=2, hidden_units=100, drop_p=0.5, k=1):
@@ -46,6 +83,7 @@ class StochasticKDropoutNet(nn.Module):
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
         return x
+
 
 if __name__ == "__main__":
 
