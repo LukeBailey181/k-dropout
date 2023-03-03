254eda13b3c1a369a8ef23c941f6f4bffb3e2547
mtp61 <mpauly@college.harvard.edu>
2023-03-03 14:34:04 -0500

    Improve efficiency and testing for k dropout modules

--- k_dropout_modules.py
+++ k_dropout_modules.py
@@ -1,3 +1,5 @@
+import time
+import numpy as np
 import torch
 from torch import nn, Tensor
 
@@ -14,20 +16,21 @@ class StochasticKDropout(nn.Module):
         super(StochasticKDropout, self).__init__()
         self.k = k
         self.p = p
+
         self.uses = 0
-        self.generator = torch.Generator()
-        self.seed = self.generator.initial_seed()
+        self.seed = torch.Generator().seed()
 
     def forward(self, x: Tensor) -> Tensor:
         if self.training:
+            g = torch.Generator(device=x.device)
             if self.uses % self.k == 0:  # update mask seed every k steps
-                self.seed = self.generator.seed()
+                self.seed = g.seed()
             else:
-                self.generator.manual_seed(self.seed)
+                g.manual_seed(self.seed)
             self.uses += 1
 
-            mask = (torch.rand(x.shape, generator=self.generator) > self.p).to(x.device)
-            return mask * x * (1.0 / (1 - self.p))  # mask and scale
+            mask = torch.rand(x.shape, device=x.device, generator=g) >= self.p
+            return (1.0 / (1 - self.p)) * (mask * x)  # mask and scale
         return x
 
     def extra_repr(self) -> str:
@@ -49,22 +52,25 @@ class PoolKDropout(nn.Module):
         super(PoolKDropout, self).__init__()
         self.n_masks = n_masks
         self.p = p
-        self.generator = torch.Generator()
-        self.mask_seeds = [self.generator.seed() for _ in range(n_masks)]
+
+        g = torch.Generator()
+        self.mask_seeds = [g.seed() for _ in range(n_masks)]
 
     def forward(self, x: Tensor) -> Tensor:
         if self.training:
             seed_index = torch.randint(high=self.n_masks, size=(1,)).item()
-            self.generator.manual_seed(self.mask_seeds[seed_index])
+            g = torch.Generator(device=x.device)
+            g.manual_seed(self.mask_seeds[seed_index])
 
-            mask = (torch.rand(x.shape, generator=self.generator) > self.p).to(x.device)
-            return mask * x * (1.0 / (1 - self.p))  # mask and scale
+            mask = torch.rand(x.shape, device=x.device, generator=g) >= self.p
+            return (1.0 / (1 - self.p)) * (mask * x)  # mask and scale
         return x
 
     def extra_repr(self) -> str:
         return f'p={self.p}, n_masks={self.n_masks}'
 
 
+'''
 class RRKDropout(nn.Module):
     r"""
     Module for the round-robin variant of k-dropout where a pool of n_masks masks are
@@ -98,3 +104,80 @@ class RRKDropout(nn.Module):
 
     def extra_repr(self) -> str:
         return f'p={self.p}, n_masks={self.n_masks}, k={self.k}'
+'''
+
+
+if __name__ == '__main__':
+    trials = 100
+    array_size = 1_000_000
+    devices = ['cpu'] + ['cuda'] if torch.cuda.is_available() else []
+    rtol = .01
+
+    # test correctness
+    # stochastic k-dropout
+    ps = [0, .25, .5, .75]
+    ks = [1, 2, 3, 5]
+    for device in devices:
+        for k in ks:
+            for p in ps:
+                skd = StochasticKDropout(k=k, p=p)
+                print(f'StochasticKDropout: device={device} k={k} p={p}')
+                for t in range(trials):
+                    x = torch.ones((array_size,), device=device)
+                    out = skd(x).cpu()
+                    p_out = 1 - ((out != 0).sum().item() / array_size)
+                    if t % k == 0:
+                        mask = out != 0
+
+                    assert np.isclose(p, p_out, rtol=rtol)  # p correct
+                    assert np.isclose(out.max().item(), 1/(1-p))  # scaling correct
+
+    # pool k-dropout
+    ps = [0, .25, .5, .75]
+    n_masks = [1, 2, 3, 5]
+    for device in devices:
+        for n_mask in n_masks:
+            for p in ps:
+                skd = PoolKDropout(n_masks=n_mask, p=p)
+                print(f'PoolKDropout: device={device} n_masks={n_mask} p={p}')
+                masks = []
+                for t in range(trials):
+                    x = torch.ones((array_size,), device=device)
+                    out = skd(x).cpu()
+                    p_out = 1 - ((out != 0).sum().item() / array_size)
+                    mask = out != 0
+                    for m in masks:
+                        if (m == mask).all().item():
+                            break
+                    else:
+                        masks.append(mask)
+
+                    assert np.isclose(p, p_out, rtol=rtol)  # p correct
+                    assert np.isclose(out.max().item(), 1/(1-p))  # scaling correct
+                # mask pool correct
+                if p == 0:
+                    assert len(masks) == 1
+                else:
+                    assert len(masks) == n_mask
+
+    # test performance
+    # NOTE: we expect nn.Dropout to outperform our layer, we just need this gap
+    #       to not be worse on the gpu
+    input_size = 100_000
+    n_trials = 10_000
+
+    layers = [
+            nn.Dropout(p=.5),
+            StochasticKDropout(k=10, p=.5),
+            PoolKDropout(n_masks=100, p=.5),
+            ]
+
+    for device in devices:
+        input = torch.randn((input_size,), device=device)
+        for layer in layers:
+            start_time = time.perf_counter()
+
+            for _ in range(n_trials):
+                out = layer(input)
+
+            print(f'{layer}, device={device}: {time.perf_counter() - start_time:.2f}s')
