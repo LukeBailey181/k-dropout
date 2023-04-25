c37376a2956c78c644f96ed0494a3f548e754fbc
Ubuntu <ubuntu@ip-172-31-1-183.ec2.internal>
2023-04-25 15:29:01 +0000

    Finish non ensemble pool subnet analysis

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import torch
+import random 
+import numpy as np
 from torch.nn import Sequential
 import wandb 
 from statistics import mean
@@ -6,7 +8,7 @@ import argparse
 
 from k_dropout.experiment_helpers import DATASETS, get_dataset
 from k_dropout.networks import PoolDropoutLensNet
-from k_dropout.training_helpers import test_net
+from k_dropout.training_helpers import test_net, train_net
 """
 except ModuleNotFoundError:
     from experiment_helpers import DATASETS, get_dataset
@@ -21,7 +23,10 @@ BATCH_SIZE = 512
 def plot_train_loss():
     pass
 
-def evaluate_subnets_in_pooled_dropout_net(path_to_model, num_subnets, test_set):
+def evaluate_subnets_in_pooled_dropout_net(path_to_model, num_subnets, test_set, run_name, wandb_config):
+
+    # TODO add support for local run
+    run = wandb.init(project="k-dropout", config=config, name=run_name)
 
     # Init lens net using the loaded model
     net = torch.load(path_to_model)
@@ -85,14 +90,18 @@ def evaluate_subnets_in_pooled_dropout_net(path_to_model, num_subnets, test_set)
         y_label="Mean Test Accuracy", 
         x_vals=["Pool Subnet", "Random Dropout Subnet", "Pool Net"],
         y_vals=[mean_pool_subnet, mean_random_subnet, full_test_acc],
-        plot_type="scatter"
+        plot_type="bar"
     )
 
 
-def evaluate_subnets_in_dropout_net(path_to_model, num_subnets, test_set):
+def evaluate_subnets_in_dropout_net(path_to_model, num_subnets, test_set, run_name, wandb_config):
+
+    # TODO add support for local run
+    run = wandb.init(project="k-dropout", config=config, name=run_name)
 
     # Init lens net using the loaded model
     net = torch.load(path_to_model)
+    net.train()
 
     dropout_subnet_accs = []
     for _ in range(num_subnets): 
@@ -101,7 +110,10 @@ def evaluate_subnets_in_dropout_net(path_to_model, num_subnets, test_set):
         dropout_subnet_accs.append(test_acc)
 
     subnet_idxs = list(range(num_subnets))
-    # Do wandb logging for random subnets
+    net.eval()
+    _, full_test_acc = test_net(net, test_set, eval_net=True)
+
+    # Do wandb logging 
     wandb_plot(
         "dropout_subnet_accs", 
         "Dropout Subnet Test Accuracy",
@@ -111,6 +123,42 @@ def evaluate_subnets_in_dropout_net(path_to_model, num_subnets, test_set):
         dropout_subnet_accs,
         "scatter"
     )
+    wandb_plot(
+        plot_id="pool_subnets_summary", 
+        plot_title="Pool Subnet Summary",
+        x_label="Net Type",
+        y_label="Mean Test Accuracy", 
+        x_vals=["Dropout Net Subnet", "Dropout Net"],
+        y_vals=[mean(dropout_subnet_accs), full_test_acc],
+        plot_type="bar"
+    )
+
+def evaluate_ensemble_of_subnets(path_to_model, num_subnets, test_set, train_set, epochs, lr, seed, device):
+    # seed experiment
+    # TODO just move to __main__
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    # Init lens net using the loaded model
+    net = torch.load(path_to_model)
+    lens_net = PoolDropoutLensNet(init_net=net)
+
+    independent_subnet_accs = []
+    for subnet_idx in range(num_subnets):
+        lens_net.reset_weights()
+        lens_net.freeze_mask(subnet_idx)
+
+        train_net(
+            net=net,
+            train_set=train_set,
+            test_set=test_set,
+            epochs=epochs,
+            lr=lr,
+            device=device,
+            use_wandb=True, #TODO handle local runs
+        )
+
 
 
 def wandb_plot(plot_id, plot_title, x_label, y_label, x_vals, y_vals, plot_type):
@@ -198,10 +246,7 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-
-    # TODO add support for local run
     config = vars(args)
-    run = wandb.init(project="k-dropout", config=config, name=args.run_name)
 
     # get dataset
     _, test_set = get_dataset(
@@ -217,7 +262,15 @@ if __name__ == "__main__":
         evaluate_subnets_in_pooled_dropout_net(
             path_to_model=args.path_to_model,             
             num_subnets=args.num_subnets,
-            test_set=test_set
+            test_set=test_set,
+            run_name=args.run_name,
+            wandb_config=config
         )
     elif args.experiment_type == "standard_dropout_subnets":
-        raise NotImplementedError("Standard dropout subnet experiment not implemented yet")
+        evaluate_subnets_in_dropout_net(
+            path_to_model=args.path_to_model, 
+            num_subnets=args.num_subnets, 
+            test_set=test_set,
+            run_name=args.run_name,
+            wandb_config=config
+        )
