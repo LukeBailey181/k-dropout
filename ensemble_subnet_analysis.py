c9ab8879de1e23501e147c2ea96f6e0c1cef82aa
Ubuntu <ubuntu@ip-172-31-1-183.ec2.internal>
2023-05-03 16:52:22 +0000

    Update ensemble experiments. TODO clean up

--- ensemble_subnet_analysis.py
+++ ensemble_subnet_analysis.py
@@ -5,6 +5,7 @@ from torch.nn import Sequential
 import wandb
 from statistics import mean, mode
 import argparse
+from tqdm import tqdm
 
 from k_dropout.experiment_helpers import DATASETS, get_dataset
 from k_dropout.networks import PoolDropoutLensNet
@@ -49,45 +50,143 @@ def evaluate_ensemble_of_subnets(
     if path_to_save_model is not None:
         torch.save(lens_net.net, path_to_save_model + f"_{subnet_idx}")
 
-def evaluate_ensemble(
+@torch.no_grad()
+def evaluate_pooled_and_dropout_subnet_ensemble(
+    path_to_pool_model, 
+    path_to_dropout_model,
+    test_set,
+    num_subnets,
+):
+
+    net = torch.load(path_to_pool_model)
+    lens_net = PoolDropoutLensNet(init_net=net)
+
+    drop_net = torch.load(path_to_dropout_model)
+    # Hacky way to use same masks
+    tmp_net = torch.load(path_to_pool_model)
+    dropout_lens_net = PoolDropoutLensNet(init_net=tmp_net) # dropout lens net has pooled_layers
+    dropout_lens_net.activate_random_masking()
+    dropout_lens_net.net = drop_net # replace net 
+    dropout_lens_net.deactivate_random_masking() # put the pooled layers back in
+
+    num_correct = 0
+    num_dropout_correct = 0
+    total_examples = 0
+    for X, y in tqdm(test_set):
+        # Make sure batch size is 1
+        preds = []
+        dropout_preds = []
+        for subnet_idx in range(num_subnets):
+            # Apprend predictions to preds
+            lens_net.freeze_mask(subnet_idx)
+            dropout_lens_net.freeze_mask(subnet_idx)
+
+            output, dropout_output = lens_net(X), dropout_lens_net(X)
+            preds.append(output.argmax(dim=1)[None])
+            dropout_preds.append(dropout_output.argmax(dim=1)[None])
+        
+        preds = torch.mode(torch.concatenate(preds, dim=0), dim=0).values 
+        dropout_preds = torch.mode(torch.concatenate(dropout_preds, dim=0), dim=0).values 
+
+        num_correct += (preds == y).sum().item() 
+        num_dropout_correct += (dropout_preds == y).sum().item()
+        total_examples += y.shape[0] 
+
+    return num_correct/total_examples, num_dropout_correct/total_examples
+
+
+def evaluate_independent_ensemble(
     path_to_models_dir, 
     model_prefix,
     num_models,
-    test_set
+    test_set,
+    device
 ):
-    
+
     models = []
     for subnet_idx in range(num_models):
         # Get string 
         path = path_to_models_dir + model_prefix + f"_{subnet_idx}"
-        net = torch.load(path)
+        net = torch.load(path, map_location="cpu")
+        print(f"Got net {subnet_idx}")
         lens_net = PoolDropoutLensNet(init_net=net)
         lens_net.freeze_mask(subnet_idx)
 
-        models.append(torch.load(lens_net))
+        models.append(lens_net.to(device))
 
     # Evaluate ensemble
-    with torch.no_grad():
+    print("Evaluating")
 
-        num_correct = 0
-        total_examples = 0
+    return(evaluate_ensemble(models, test_set))
 
-        for X, y in test_set:
-            # Make sure batch size is 1
-            assert(X.shape[0] == 1)
-            preds = []
-            for lens_net in models:
-                # Apprend predictions to preds
-                output = lens_net(X)
-                preds.append(output.argmax(dim=1).item())
-            
-            pred = mode(preds)
-            num_correct += pred == y.item() 
-            total_examples += 1 
+@torch.no_grad()
+def evaluate_ensemble(
+    models,
+    test_set,
+):
+    
+    num_correct = 0
+    total_examples = 0
 
-        print(num_correct/total_examples)
+    for X, y in tqdm(test_set):
+        # Make sure batch size is 1
+        preds = []
+        for lens_net in models:
+            # Apprend predictions to preds
+            output = lens_net(X)
+            preds.append(output.argmax(dim=1)[None])
+        
+        preds = torch.mode(torch.concatenate(preds, dim=0), dim=0).values 
+
+        num_correct += (preds == y).sum().item() 
+        total_examples += y.shape[0] 
+
+    return num_correct/total_examples
+
+# TODO break this out into own file so it 
+# is runable using command line arguments
+def run_full_ensemble_experiment():
+
+    train_set, test_set = get_dataset(
+        dataset_name="cifar10",
+        batch_size=512,
+        test_batch_size=512,
+        num_workers=4,
+        preprocess_dataset=True,
+        device="cuda",
+    )
+    print("Got test set")
+
+    pooled_subnet_ensemble_acc = []
+    independent_ensemble_acc = []
+    dropout_ensemble_acc = []
+    for i in range(1, 51):
+
+        acc, dropout_acc = evaluate_pooled_and_dropout_subnet_ensemble(
+            path_to_pool_model="./models/cifar10_ps_50.pt", 
+            path_to_dropout_model="./models/cifar10_dropout.pt", 
+            test_set=test_set,
+            num_subnets=i,
+        )
+        pooled_subnet_ensemble_acc.append(acc)
+        dropout_ensemble_acc.append(dropout_acc)
+
+        acc = evaluate_independent_ensemble(
+            path_to_models_dir="./models/ensemble/cifar10/",
+            model_prefix="cifar10_ensemble_subnet_subnet",
+            num_models=i,
+            test_set=test_set,
+            device="cuda"
+        )
+
+        independent_ensemble_acc.append(acc)
+
+    print(pooled_subnet_ensemble_acc)
+    print(independent_ensemble_acc)
+    print(dropout_ensemble_acc)
 
 if __name__=="__main__":
+
     # TODO do some arg parsing here
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
@@ -177,21 +276,3 @@ if __name__=="__main__":
         seed=args.seed,
         path_to_save_model=args.path_to_save_model,
     )
-
-
-    """
-    train_set, test_set = get_dataset(
-        dataset_name="cifar10",
-        batch_size=1,
-        test_batch_size=1,
-        num_workers=4,
-        preprocess_dataset=True,
-        device="cuda",
-    )
-    evaluate_ensemble(
-        path_to_models_dir="./models/ensemble/cifar10",
-        model_prefix="cifar10_ensemble_subnet_subnet",
-        num_models=50,
-        test_set=test_set
-    )
-    """
